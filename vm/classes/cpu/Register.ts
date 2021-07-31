edf8cfbb3bbd68329163ea814e41d1859f17fda3
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-01 00:24:46 +0300

    vm : Register class implemented

---
+++ vm/classes/cpu/Register.ts
@@ -0,0 +1,34 @@
+import IRegister from "../../interfaces/IRegister";
+
+/**
+ * This VM has 11 registers
+ * IR and CCR registers are 8 bits registers
+ * All the other registers are 16 bits registers
+ */ 
+
+class Register implements IRegister
+{
+    //Properties
+    private view:DataView;
+
+    //Methods
+    constructor(sizeInBytes:number = 2)
+    {
+        const arrayBuffer = new ArrayBuffer(sizeInBytes);
+        this.view = new DataView(arrayBuffer);
+    }
+
+
+    public read     = (): number => (this.view.byteLength === 1)? this.view.getUint8(0) : this.view.getUint16(0);
+    public readMSB  = (): number => this.view.getUint8(0);
+    public readLSB  = (): number => (this.view.byteLength === 1)? this.view.getUint8(0) : this.view.getUint8(1);
+    public write    = (data:number): void => (this.view.byteLength ===1)? this.view.setUint8(0,data) : this.view.setUint16(0,data);
+    public writeMSB = (data:number): void => this.view.setInt8(0,data);
+    public writeLSB = (data:number): void => (this.view.byteLength === 1)? this.view.setUint8(0,data) : this.view.setUint8(1,data);;
+
+    //Debug
+    public debug = ():void => console.log(this.view);
+    
+}
+
+export default Register;
---
+++ vm/interfaces/IRegister.ts
@@ -0,0 +1,11 @@
+interface IRegister
+{
+    read():number; // reads the data stored inside the register
+    readMSB():number; // reads the most significant byte of the register
+    readLSB():number; // reads the least significant byte of the register
+    write(data:number):void // writes the data into the register
+    writeMSB(data:number):void // writes the data into the most significant byte of the register 
+    writeLSB(data:number):void // writes the data into the least significant byte of the register 
+}
+
+export default IRegister;
