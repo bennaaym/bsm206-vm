d1d8c9ca3afdb470c16b56562edd49a307412cad
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-01 21:33:45 +0300

    Instructions enumerator

---
+++ vm/enums/EIDEC.ts
@@ -0,0 +1,15 @@
+/**
+ * 60 different instructions can be defined on this VM 
+ *
+ */
+
+enum EIDEC
+{
+    LDA     = 0B01010,      // assigns the operand to the accumulator
+    STA     = 0B10000,      // writes the value of the accumulator into memory
+    NOP     = 0B11001,      // no operation, the program counter(PC) will be incremented by 1
+    HLT     = 0B01110,      // stops the program
+}
+
+
+export default EIDEC;
