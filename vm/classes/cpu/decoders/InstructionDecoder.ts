a96c6328757521cf9bed9ccf104ae73f6f030c47
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-01 21:45:49 +0300

    vm : InstructionDecoder class implemented

---
+++ vm/classes/cpu/decoders/InstructionDecoder.ts
@@ -0,0 +1,28 @@
+import EIDEC from "../../../enums/EIDEC";
+import IDecoder from "../../../interfaces/cpu/IDecoder";
+
+/**
+ * In this VM the  0th, 1st, 2nd, 3rd ,and 7th bits of the opcode (reading from right to left) represent the instruction
+ * 
+ */
+
+class InstructionDecoder implements IDecoder
+{
+    // Methods
+    public decode = (opcode:number): number =>
+    {
+        const strOpcde = opcode.toString(2).padStart(8,'0');
+        const IDEC:number = parseInt(strOpcde[0] + strOpcde.substr(4,7),2);
+
+        switch(IDEC)
+        {
+            case EIDEC.LDA          :   return EIDEC.LDA;
+            case EIDEC.STA          :   return EIDEC.STA;
+            case EIDEC.HLT          :   return EIDEC.HLT;
+            default                 :   return EIDEC.NOP;
+        }
+    }
+}
+
+
+export default InstructionDecoder;
