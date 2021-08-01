35f4e43b10ac34c85bd6b44c157b30cc69b96da2
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-01 23:42:30 +0300

    vm : LDA instruction implemented for the immediate addressing mode

--- vm/classes/cpu/executor/ImmediateModeExecutor.ts
+++ vm/classes/cpu/executor/ImmediateModeExecutor.ts
@@ -1,28 +1,79 @@
+import EIDEC from "../../../enums/EIDEC";
+import EREG from "../../../enums/EREG";
 import IExecutor from "../../../interfaces/cpu/IExecutor";
+import IRegisters from "../../../interfaces/cpu/IRegisters";
 import IMemory from "../../../interfaces/memory/IMemory";
+import Memory from "../../memory/Memory";
+import Registers from "../Registers";
 
 class ImmediateModeExecutor implements IExecutor
 {
     // Properties
     private IDEC:number;
-    private memoryRef:IMemory;
-    private registersRef:IMemory;
-    
+    private memoryRef : IMemory;
+    private registersRef :IRegisters;
+
     // Constructor  
     constructor(IDEC:number)
     {
         this.IDEC = IDEC;
+        this.memoryRef = Memory.getInstance();
+        this.registersRef = Registers.getInstance();
     }
 
     // Methods
     public execute = (): void =>
     {
-
+        switch(this.IDEC)
+        {
+            case EIDEC.LDA:
+                this.LDA();
+                break;
+        }
     };
 
 
     
+    public LDA = () =>
+    {
+        this.commonSteps();
 
+        // T5 : AC <- DR
+        const DR = this.registersRef.getRegister(EREG.DR);
+        this.registersRef.getRegister(EREG.AC).write(DR.read());
+    }
+
+
+
+    
+
+
+
+    // Phases 
+
+    public commonSteps = () =>
+    {
+        this.T3();
+        this.T4();
+    }
+
+    public T3 = (): void =>
+    {
+        // T3 : DR_H <- M[AR], AR <- AR + 1 
+        const AR = this.registersRef.getRegister(EREG.AR);
+        const data = this.memoryRef.read(AR.read());
+        this.registersRef.getRegister(EREG.DR).writeMSB(data);
+        AR.increment();
+    }
+
+    public T4 = (): void =>
+    {
+        // T4 : DR_L <- M[AR], PC <- PC + 1
+        const AR = this.registersRef.getRegister(EREG.AR);
+        const data = this.memoryRef.read(AR.read());
+        this.registersRef.getRegister(EREG.DR).writeLSB(data);
+        this.registersRef.getRegister(EREG.PC).increment();
+    }
 }
 
 export default ImmediateModeExecutor;
---
+++ vm/main.ts
@@ -0,0 +1,19 @@
+// import ControlUnit from "./classes/cpu/ControlUnit";
+// import Memory from "./classes/memory/Memory";
+// import Registers from "./classes/cpu/Registers";
+
+
+// const cu = ControlUnit.getInstance();
+// const meo = Memory.getInstance();
+// const regs =Registers.getInstance();
+
+
+// meo.write(0x0000,0x1A);
+// meo.write(0x0001,0xDC);
+// meo.write(0x0002,0xBA);
+
+// cu.fetch();
+// const {ADRMD,IDEC} = cu.decode();
+// cu.execute(ADRMD,IDEC);
+
+// regs.debug();
