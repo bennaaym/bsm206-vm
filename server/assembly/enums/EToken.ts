1121a844803b2caed9a4f212a7ba8a1b14207192
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-13 11:42:58 +0300

    assembly: EToken enumerator

---
+++ server/assembly/enums/EToken.ts
@@ -0,0 +1,10 @@
+enum EToken
+{
+    MNEMONIC    = 'MNEMONIC', 
+    ADRMD       = 'ADRMD',      // addressing mode
+    OPERAND     = 'OPERAND', 
+    NL          = 'NL',         // new line
+    EOF         = 'EOF'         // end of file
+}
+
+export default EToken;
