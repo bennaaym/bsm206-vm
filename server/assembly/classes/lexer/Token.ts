10eebaf69538238f45517a768e85ccda92e6c1c6
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-13 11:33:04 +0300

    assembly: getPosition method implemented inside Token class

--- server/assembly/classes/lexer/Token.ts
+++ server/assembly/classes/lexer/Token.ts
@@ -1,22 +1,26 @@
-import IToken from "../interfaces/IToken";
+import IPosition from "../../interfaces/lexer/IPosition";
+import IToken from "../../interfaces/lexer/IToken";
 
 class Token implements IToken
 {
     // Properties
     private type:string;
     private value:string;
+    private position:IPosition;
 
     // Constructor
-    constructor(type:string,value:string)
+    constructor(type:string,value:string,position:IPosition)
     {
         this.type = type;
         this.value = value;
+        this.position = position;
     }
 
     // Methods
 
-    public getType  = (): string => this.type;
-    public getValue = (): string => this.value;
+    public getType     =   (): string => this.type;
+    public getValue    =   (): string => this.value;
+    public getPosition =   ():IPosition => this.position;
 
 }
 
--- server/assembly/interfaces/lexer/IToken.ts
+++ server/assembly/interfaces/lexer/IToken.ts
@@ -1,6 +1,9 @@
+import IPosition from "./IPosition";
+
 interface IToken 
 {
     getType():string;
     getValue():string;
+    getPosition():IPosition;
 }
 export default IToken;
