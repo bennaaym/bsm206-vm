b37abf24ba920e8022b63a8de80da2dafe8bd87e
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-14 23:42:59 +0300

    assembly : MNEMONICS, EADRMD created

---
+++ server/assembly/enums/EADRMD.ts
@@ -0,0 +1,11 @@
+enum EADRMD 
+{
+    INHERENT    = 'INHERENT',
+    IMMEDIATE   = 'IMMEDIATE',
+    DIRECT      = 'DIRECT',
+    INDIRECT    = 'INDIRECT',
+    INDEX       = 'INDEX',
+    RELATIVE    = 'RELATIVE'
+}
+
+export default EADRMD;
