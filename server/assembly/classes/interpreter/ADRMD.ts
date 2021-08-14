294352cd59c980bb13db5d8e9ff05080a15f622c
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-14 23:45:54 +0300

    assembly : ADRMD created

---
+++ server/assembly/classes/interpreter/ADRMD.ts
@@ -0,0 +1,9 @@
+export const ADRMD =
+{
+    INHERENT    : 0B000,
+    IMMEDIATE   : 0B001,  
+    DIRECT      : 0B010, 
+    INDIRECT    : 0B011,
+    INDEX       : 0B100,
+    RELATIVE    : 0B101,
+}
