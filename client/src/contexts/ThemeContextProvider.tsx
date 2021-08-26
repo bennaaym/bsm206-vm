f6e575c2a8615a04fa36d660fcdb3cc06231a83f
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-26 22:16:09 +0300

    client : ThemeContextProvider updated to read from and store theme data into localStorage

--- client/src/contexts/ThemeContextProvider.tsx
+++ client/src/contexts/ThemeContextProvider.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState, createContext, useContext, ReactNode } from "react";
 
 interface ITheme
@@ -21,6 +22,15 @@ const ThemeContextProvider:React.FC<ReactNode> = ({children}) =>
 
     const [isLight,setIsLight] = useState(true);
     const toggleTheme = () => setIsLight((current) => !current);
+
+    useEffect(()=>{
+        const saveTheme = localStorage.getItem('bsm206-vm-theme');
+        setIsLight((saveTheme === 'dark')?false:true);
+    },[])
+
+    useEffect(()=>{  
+        localStorage.setItem('bsm206-vm-theme',isLight?'light':'dark')   
+    },[isLight])
 
     return(
         <ThemeContext.Provider value={{
