bc8ffcfff693461ff35ad0b50b08cf7f780413d6
bennaaym <mr.aymen.bennabi@gmail.com>
2021-08-26 19:12:18 +0300

    client : assembly section updated

--- client/src/components/documentation/AsmLanguage.tsx
+++ client/src/components/documentation/AsmLanguage.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faCheckCircle, faTimesCircle} from "@fortawesome/free-solid-svg-icons"
 import styles from "../../assets/css/documentation.module.css";
 
 const AsmLanguage = () =>
@@ -8,10 +10,40 @@ const AsmLanguage = () =>
                 <h1>assembly language</h1>
                 <section>
                     <h4>syntax</h4>
-                    <div className="my-2 space-x-2 uppercase bg-dark-background-200 rounded p-2">
+                    <div className={styles.snippet}>
                         <span className="text-dark-keyword">mnemonic</span>
                         <span className="text-dark-number">operand</span>
                         <span className ="text-dark-comment">; comment</span>
+                    </div>
+                </section>   
+                <section className="space-y-2">
+                    <h4>numbers</h4>
+                    <p>
+                        all numbers are considered as a hexadecimal value
+                    </p>
+                    <p>
+                        if the hexadecimal value starts with a letter [A-F] an insignificant zero should be added at the beginning otherwise an error will be raised
+                    </p>
+                    <div className={styles.snippet}>
+                        <span className="text-dark-keyword">LDA</span>
+                        <span className="text-dark-number">#1234</span>
+                        <span className ="text-dark-comment">; 1234 is a hexadecimal value</span>
+                    </div>
+                    <div className={styles.snippet}>
+                        <span className="text-error text-xl">
+                            <FontAwesomeIcon icon={faTimesCircle}/>
+                        </span>
+                        <span className="text-dark-keyword">LDA</span>
+                        <span className="text-dark-text-300">#A123</span>
+                        <span className ="text-dark-comment">; an error will be raised because A123 won't be concidered as a numerical value</span>
+                    </div>
+                    <div className={styles.snippet}>
+                        <span className="text-light-comment text-xl">
+                            <FontAwesomeIcon icon={faCheckCircle}/>
+                        </span>
+                        <span className="text-dark-keyword">LDA</span>
+                        <span className="text-dark-number">#0A123</span>
+                        <span className ="text-dark-comment">; no error will be raised and A123 will be evaluated as numerical value</span>
                     </div>
                 </section>   
             </section>
--- client/src/pages/Documentation.tsx
+++ client/src/pages/Documentation.tsx
@@ -4,14 +4,14 @@ import Architecture from "../components/documentation/Architecture";
 import AsmLanguage from "../components/documentation/AsmLanguage";
 import InstructionSet from "../components/documentation/InstructionSet";
 import { useTheme } from "../contexts/ThemeContextProvider";
-
+import styles from "../assets/css/documentation.module.css";
 const Documentation = () =>
 {
     const {isLight} = useTheme()
     return(
         <>
-            <div className={`flex flex-grow w-full  justify-center py-12 ${isLight?'text-light-text-300 ':'text-dark-text-300 bg-dark-background-300'} `}>
-                <div className="w-11/12 sm:w-10/12 md:w-8/12 space-y-16">
+            <div className={`${styles['documentation']} ${isLight?'text-light-text-300 ':'text-dark-text-300 bg-dark-background-300'} `}>
+                <div className={styles['documentation-wrapper']}>
                   <Switch>
                     <Route path='/documentation/architecture' component={Architecture}/>
                     <Route path='/documentation/addressing-modes' component={AdrModes}/>
